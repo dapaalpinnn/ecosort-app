08b45624f263dc5341519ba7d963dc43bca6f7a5
Dafa Alvin <dafaalvin10@gmail.com>
2026-05-04 08:55:32 +0700

    style: cleanup boilerplate and adjust styling

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,30 @@
+import { type VariantProps } from "class-variance-authority"
+import { Slot } from "radix-ui"
+import { buttonVariants } from "@/components/ui/button/utils"
+import { cn } from "@/lib/utils"
+import { type ComponentProps } from "react"
+
+const Button = ({
+  className,
+  variant = "default",
+  size = "default",
+  asChild = false,
+  ...props
+}: ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) => {
+  const Comp = asChild ? Slot.Root : "button"
+
+  return (
+    <Comp
+      data-slot="button"
+      data-variant={variant}
+      data-size={size}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
+
+export default Button
