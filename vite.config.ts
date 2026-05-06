dd5a1cfe0a276ee6004c6ad3c6420520895f9833
Dafa Alvin <dafaalvin10@gmail.com>
2026-05-06 08:13:57 +0700

    build: configure allowedhosts for ngrok tunnel

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ["8db0-202-154-36-233.ngrok-free.app"], // if you use ngrok, change this to your ngrok url
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
